76823b42cf4c8adad75defa95ba64c34a438af8e
Heather Mahan <heatheramahan@gmail.com>
2016-04-05 11:49:55 -0700

    Lynda tutorial 3.4 creating an svg bar chart

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-d3.select('#chart')
-	.append('svg')
-		.attr('width', 600)
-		.attr('height', 400)
-		.style('background', 'gray')
-	.append('rect')
-		.attr('x', 200)
-		.attr('y', 100)
-		.attr('width', 200)
-		.attr('height', 200)
-		.style('fill', 'darkorange')
-	d3.select('svg')
-		.append('circle')
-			.attr('cx', 300)
-			.attr('cy', 200)
-			.attr('r', 50)
-			.style('fill', 'purple');
+var data = [20, 30, 45, 15],
+	width = 600,
+	height = 400,
+	barWidth = 50,
+	barOffset = 5;
+
+d3.select('#chart').append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.style('background', 'gray')
+	.selectAll('rect').data(data).enter().append('rect')
+		.style('fill', 'magenta')
+		.attr('width', barWidth)
+		.attr('height', function(d) {
+			return d;
+		})
+		.attr('x', function(d, i) {
+			return i * (barWidth + barOffset);
+		})
+		.attr('y', function(d) {
+			return height - d;
+		});

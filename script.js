4adfed9f6dd981f6be54a368ac7a69cf17197de3
Heather Mahan <heatheramahan@gmail.com>
2016-04-05 12:41:59 -0700

    Lynda tutorial 4.1-4.3 using d3 scales.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 var data = [20, 30, 45, 15],
 	width = 600,
-	height = 400,
-	barWidth = 50,
-	barOffset = 5;
+	height = 400;
+
+var yscale = d3.scale.linear()
+			   .domain([0, d3.max(data)])
+			   .range([0, height]),
+	xscale = d3.scale.ordinal()
+			   .domain(d3.range(0, data.length))
+			   .rangeBands([0, width]),
+	colors = d3.scale.linear()
+			   .domain([0, d3.max(data)])
+			   .range(['orange', 'magenta']);
 
 d3.select('#chart').append('svg')
 	.attr('width', width)
 	.attr('height', height)
 	.style('background', 'gray')
 	.selectAll('rect').data(data).enter().append('rect')
-		.style('fill', 'magenta')
-		.attr('width', barWidth)
+		.style('fill', colors)
+		.attr('width', xscale.rangeBand())
 		.attr('height', function(d) {
-			return d;
+			return yscale(d);
 		})
 		.attr('x', function(d, i) {
-			return i * (barWidth + barOffset);
+			return xscale(i);
 		})
 		.attr('y', function(d) {
-			return height - d;
+			return height - yscale(d);
 		});

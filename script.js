f5f0aa30b6b3537849be61cf3d743465d036a6fc
Heather Mahan <heatheramahan@gmail.com>
2016-04-05 12:52:05 -0700

    Lynda tutorial 4.3 left to right color scales

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var data = [20, 30, 45, 15],
+var data = [],
 	width = 600,
 	height = 400;
+
+for (var i = 0; i < 100; i++) {
+	data.push(Math.random());
+}
 
 var yscale = d3.scale.linear()
 			   .domain([0, d3.max(data)])
@@ -9,15 +13,17 @@ var yscale = d3.scale.linear()
 			   .domain(d3.range(0, data.length))
 			   .rangeBands([0, width]),
 	colors = d3.scale.linear()
-			   .domain([0, d3.max(data)])
-			   .range(['orange', 'magenta']);
+			   .domain([0, data.length*0.25, data.length*0.5, data.length*0.75, data.length])
+			   .range(['red', 'orange', 'yellow', 'green', 'blue']);
 
 d3.select('#chart').append('svg')
 	.attr('width', width)
 	.attr('height', height)
 	.style('background', 'gray')
 	.selectAll('rect').data(data).enter().append('rect')
-		.style('fill', colors)
+		.style('fill', function(d, i) {
+			return colors(i);
+		})
 		.attr('width', xscale.rangeBand())
 		.attr('height', function(d) {
 			return yscale(d);
